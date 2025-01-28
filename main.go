47ce4b1c524740cb32b3dcd1f3458c8bc6a13f3d
jbrinkman <joe.brinkman@improving.com>
2025-01-28 09:35:36 -0500

    add MIT license
    Signed-off-by: jbrinkman <joe.brinkman@improving.com>

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"github.com/jbrinkman/go-wordcloud/cmd"
-	"log"
+	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		log.Println(err)
+	var rootCmd = &cobra.Command{
+		Use:   "go-wordcloud",
+		Short: "A CLI tool to generate word clouds",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Welcome to the Word Cloud Generator!")
+			// Add additional functionality here
+		},
+	}
+
+	// Initialize Viper for configuration management
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s", err)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
